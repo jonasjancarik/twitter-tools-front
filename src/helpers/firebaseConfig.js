e82ce2c55f5e97bf134d61a55c766b5671ea3f08
Jonáš Jančařík <jonas.jancarik@gmail.com>
2019-02-22 14:25:52 +0100

    Update firebaseConfig.js

--- src/helpers/firebaseConfig.js
+++ src/helpers/firebaseConfig.js
@@ -1,8 +1,8 @@
 export const config = {
-  apiKey: 'AIzaSyAQiZoz3-EqYSIwe0fdcru0HhF66N1Ok8U',
-  authDomain: 'bookmarks-fe73e.firebaseapp.com',
-  databaseURL: 'https://bookmarks-fe73e.firebaseio.com',
-  projectId: 'bookmarks-fe73e',
-  storageBucket: 'bookmarks-fe73e.appspot.com',
-  messagingSenderId: '880130415659'
+  apiKey: 'AIzaSyDi-CJIi1nz6j78YXS2BbWd7gHux_e-Mb4',
+  authDomain: 'listblendr.firebaseapp.com',
+  databaseURL: 'https://listblendr.firebaseio.com',
+  projectId: 'listblendr',
+  storageBucket: 'listblendr.appspot.com',
+  messagingSenderId: '28253641814'
 }
